42f282af7bb72d420a9da4311b5bc1b5c1b81192
Shivane Rana <shivanerana2004@gmail.com>
2024-08-25 16:19:59 +0530

    logic for rendering the gameboard on console created

---
+++ script.js
@@ -0,0 +1,27 @@
+// const arr = [[0,0,0],[0,0,0],[0,0,0]];
+
+
+// function renderArray(arr){
+// 	let pattern = "";
+// 	for(let i in arr){
+// 		pattern += "\n";
+// 		pattern += arr[i];
+// 	}
+// 	pattern += '\n';
+// 	console.log(pattern);
+// }
+
+// function editArray(arr,postion1,postion2){
+// 	let choice = arr[postion1][postion2];
+// 	if(choice === 0){
+// 		arr[postion1][postion2] = 1;
+// 	}else if(choice === 1){
+// 		arr[postion1][postion2] = 0;
+// 	}
+// }
+
+// editArray(arr,0,0);
+// editArray(arr,1,1);
+// editArray(arr,2,2);
+
+// renderArray(arr);
