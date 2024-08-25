7dd886b29b15fab2e2961e54601e55908d2d7cc4
Shivane Rana <shivanerana2004@gmail.com>
2024-08-25 22:16:49 +0530

    more edit to logic

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-// // this is the real shit 
-// function gameBoard(){
-// 	const arr = [[" "," "," "],[" "," ",""],[" "," "," "]];
+// this is the real shit 
+function gameBoard(){
+	const arr = [[" "," "," "],[" "," ",""],[" "," "," "]];
 	
-// 	//To render the gameboard
-// 	function renderArray(){
-// 		let pattern = "";
-// 		for(let i in arr){
-// 			pattern += "\n";
-// 			pattern += arr[i];
-// 		}
-// 			pattern += '\n';
-// 			console.log(pattern);
-// 		}
+	//To render the gameboard
+	function renderArray(){
+		let pattern = "";
+		for(let i in arr){
+			pattern += "\n";
+			pattern += arr[i];
+		}
+			pattern += '\n';
+			console.log(pattern);
+		}
 		
-// 	//To edit the gameboard
-// 	function editArray(postion1,postion2){
-// 		let choice = arr[postion1][postion2];
-// 		if(choice === " "){
-// 			arr[postion1][postion2] = "X";
-// 		}else if(choice === "X"){
-// 			arr[postion1][postion2] = "O";
-// 		}
-// 	}
-	
-// 	return {editArray,renderArray};
-	
-// }
+	//To edit the gameboard
+	function editArray(postion1,postion2){
+		let choice = arr[postion1][postion2];
+        if(choice === " "){
+         		arr[postion1][postion2] = "X";   
+        }else if(choice === "X"){
+						arr[postion1][postion2] = "O";
+		}
 
-// const game1 = gameBoard();
+        renderArray();
+	}
+	
+	return {editArray,renderArray};
+	
+}
+
+const game1 = gameBoard();
+game1.renderArray()
+game1.editArray(1,1)
+game1.editArray(0,1);
+game1.editArray(2,1);
