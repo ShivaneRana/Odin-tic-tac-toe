b071f6beb508f9035f27dc20cfb2a61a787adceb
Shivane Rana <shivanerana2004@gmail.com>
2024-08-26 20:59:04 +0530

    pattern creating logic changed

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 function gameBoard(){
-	const arr = [[" "," "," "],[" "," "," "],[" "," "," "]];
+	const arr = [" "," "," "," "," "," "," "," "," "];
 	
 	const render = function(){
 		let pattern = "\n";
 		for(let i = 0;i<arr.length;i++){
 			pattern += arr[i];
-			pattern += "\n";
+			if(pattern.length % 4 === 0){
+				pattern += "\n"
+			}
 		}
 		console.log(pattern);
 	}
 	
-	const edit = function(pos1,pos2,symbol){
-	const choice = arr[pos1][pos2];
+	const edit = function(pos,symbol){
+	const choice = arr[pos];
 	if(choice !== " "){
 		console.log("No changes were made");	
 	}else{
-		arr[pos1][pos2] = symbol;
+		arr[pos] = symbol;
 	}}
 	
 	return {render,edit}
 }
+
+const game1 = gameBoard();
+game1.edit(0,"X");
+game1.edit(1,"X");
+game1.edit(2,"X");
+game1.render();
