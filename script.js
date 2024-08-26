dd5720fda9225ae8b88fda3aed17c66df2e2547a
Shivane Rana <shivanerana2004@gmail.com>
2024-08-26 20:37:03 +0530

    recreting the project

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-// this is the real shit 
 function gameBoard(){
-	const arr = [[" "," "," "],[" "," ",""],[" "," "," "]];
+	const arr = [[" "," "," "],[" "," "," "],[" "," "," "]];
 	
-	//To render the gameboard
-	function render(){
-		let pattern = "";
-		for(let i in arr){
-			pattern += "\n";
+	const render = function(){
+		let pattern = "\n";
+		for(let i = 0;i<arr.length;i++){
 			pattern += arr[i];
+			pattern += "\n";
 		}
-			pattern += '\n';
-			console.log(pattern);
-		}
-		
-	//To edit the gameboard
-	function edit(postion1,postion2,symbol){
-		let choice = arr[postion1][postion2];
-    if(choice !== " "){
-			console.log("Cannot overwritte value");
-    }else{
-			arr[postion1][postion2] = symbol;				
-		}
-		render();
-		}
+		console.log(pattern);
+	}
 	
-	return {edit,render};
+	const edit = function(pos1,pos2,symbol){
+	const choice = arr[pos1][pos2];
+	if(choice !== " "){
+		console.log("No changes were made");	
+	}else{
+		arr[pos1][pos2] = symbol;
+	}}
 	
+	return {render,edit}
 }
-
-function player1(name = "Player1"){
-	const symbol = "X";
-	return {name,symbol};
-}
-
-function player2(name = "Player2"){
-	const symbol = "O";
-	return {name,symbol}
-}
-
-const game1 = gameBoard();
-const p1 = player1("Shivane");
-const p2 = player2("Anuj");
-
-game1.render();
